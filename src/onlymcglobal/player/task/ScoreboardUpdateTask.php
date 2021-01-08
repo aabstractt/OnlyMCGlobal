71dac3e000d4fbfc22b76490a9eab9011dd81aea
iTheTrollIdk <xavier04.martinez@gmail.com>
2021-01-08 09:03:24 -0600

    idiot

--- src/onlymcglobal/player/task/ScoreboardUpdateTask.php
+++ src/onlymcglobal/player/task/ScoreboardUpdateTask.php
@@ -4,8 +4,10 @@ declare(strict_types=1);
 
 namespace onlymcglobal\player\task;
 
+use libBungeeCore\BungeeCore;
 use onlymcglobal\OnlyMCGlobal;
 use onlymcglobal\player\Player;
+use onlymcglobal\translation\TranslationFactory;
 use pocketmine\scheduler\Task;
 use pocketmine\Server;
 
@@ -17,10 +19,16 @@ class ScoreboardUpdateTask extends Task {
     public function onRun(): void {
         foreach (Server::getInstance()->getOnlinePlayers() as $player) {
             /** @var Player $player */
-            OnlyMCGlobal::getScoreboard()->setLines([$player], [
-                10 => ' Conexión: &a' . $player->getNetworkSession()->getPing(),
-                4 => ' Conectados: &a' . count(Server::getInstance()->getOnlinePlayers())
-            ]);
+            $rankString = OnlyMCGlobal::getDefaultScoreboardFormat();
+
+            OnlyMCGlobal::getScoreboard()->setLines([$player], TranslationFactory::getInstance()->translateArray('LOBBY_SCOREBOARD_UPDATE', [
+                $rankString,
+                $player->getNetworkSession()->getPing(),
+                10,
+                3,
+                BungeeCore::getServerId(),
+                count(Server::getInstance()->getOnlinePlayers())
+            ]));
         }
     }
 }
